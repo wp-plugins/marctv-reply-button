7c50ec1c938311939ae2c4d91125e6bd212d3d6c
MarcDK <MarcDK@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-04 20:19:15 +0000

    Tagging 1.7.2
    git-svn-id: https://plugins.svn.wordpress.org/marctv-reply-button/tags/1.7.2@980456 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- marctv_reply_button.php
+++ marctv_reply_button.php
@@ -1,10 +1,10 @@
 <?php
 /*
   Plugin Name: MarcTV Reply Button
-  Plugin URI: http://www.marctv.de/blog/2010/08/25/marctv-wordpress-plugins/
-  Description: Adds a reply button after every 2nd level thread with jQuery. Works only with threaded comment 2nd level settings.
-  Version: 1.6
-  Author: Marc Tönsing
+  Plugin URI: http://www.marctv.de/blog/marctv-wordpress-plugins/
+  Description: Places an additional reply button at the end of the comment thread. 
+  Version: 1.7.2
+  Author: MarcDK
   Author URI: http://www.marctv.de
   License: GPL2
 
@@ -18,11 +18,11 @@
 function add_marctv_reply_scripts () {
     wp_enqueue_script(
         "marctv_reply_button", WP_PLUGIN_URL . "/marctv-reply-button/marctv_reply_button.js",
-        array("jquery"), "1.6", 0
+        array("jquery"), "1.7.1", 0
     );
 }
 
-if (!is_admin() ) {
+if (!is_admin()) {
   add_action('wp_print_styles', 'add_marctv_reply_scripts');
 }
 
